ee93511c1806aeaf3cc593a5307483b88126828b
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-02-01 09:53:01 +0100

    Adding readme and refactoring code a little bit.

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from parsing import *
 from sources.constraint_programming import constraint_programming
 import time
 
+
 def getListOfCellsInASequence(sequence):
     """Take a sequence and returns its cells under the format of variable"""
     L = []
@@ -15,12 +16,14 @@ def getListOfCellsInASequence(sequence):
             L.append(((sequence[0][0] + i, sequence[0][1]),(sequence[0][0] + i, sequence[0][1])))
     return L
 
+
 def getTypeVariables(var):
     """Check if the variable is a letter or a sequence."""
     if var[0] == var[1]:
         return "letter"
     else:
         return "sequence"
+
 
 def getCorrectWordLenght(x,words):
     """It takes the lenght of a sequence and a list of words.
@@ -32,6 +35,7 @@ def getCorrectWordLenght(x,words):
             L.append(w)
     return L
 
+
 def lenSequence(x):
     """Get a variable and calculate its length"""
     if x[0][0] == x[1][0]: #If it's a row
@@ -39,6 +43,22 @@ def lenSequence(x):
     else:
         l = x[1][0] - x[0][0] + 1 #It it's a column
     return l
+
+
+def functR3(dico, letters, xj):
+    """A sequence is the concatenation of its letters.
+       i is a sequence ; j is a letter in the sequence j at the index xj
+       dico is the dictionnary of words
+       letters are the letters in the dico"""
+    #i is a sequence
+    #j is a letter in the sequence j at the index xj
+    new_dico = []
+    for elt in dico:
+        if len(elt) > xj: #We enable the sequence from the dico with a sufficient length
+            new_dico.append(elt)
+
+    R3 = { (i,j) for i in new_dico for j in letters if i[xj]==j}
+    return R3
 
 
 def main(crossword_file, crossword_dico, maintain_arc_consistency):
@@ -54,27 +74,13 @@ def main(crossword_file, crossword_dico, maintain_arc_consistency):
     for elt in var2:
         var[elt] = var2[elt]
 
-
     P = constraint_programming(var)
-
-    def functR3(xj):
-        """A sequence is the concatenation of its letters.
-           i is a sequence ; j is a letter in the sequence j at the index xj"""
-        #i is a sequence
-        #j is a letter in the sequence j at the index xj
-        new_dico = []
-        for elt in dico:
-            if len(elt) > xj: #We enable the sequence from the dico with a sufficient length
-                new_dico.append(elt)
-
-        R3 = { (i,j) for i in new_dico for j in letters if i[xj]==j}
-        return R3
 
     for a in var:
         if getTypeVariables(a) == "sequence":
             i=0
             for var_letter in getListOfCellsInASequence(a):
-                P.addConstraint(a,var_letter,functR3(i))
+                P.addConstraint(a, var_letter, functR3(dico, letters, i))
                 i += 1 #the position of the letter within the sequence
 
     if maintain_arc_consistency == "y":
