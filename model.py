2ec290b7a587ac62cd48a2519b23aae4cb3b1180
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-02-01 07:52:57 +0100

    Adding arc consistency possibility.

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def getListOfCellsInASequence(sequence):
     if sequence[0][1] == sequence[1][1]: #For a column
         a = sequence[1][0] - sequence[0][0] #get the size of the sequence
         for i in range(a + 1):
-            L.append(((sequence[0][0] + i, sequence[0][1]),(sequence[0][0] + i, sequence[0][1])))  
+            L.append(((sequence[0][0] + i, sequence[0][1]),(sequence[0][0] + i, sequence[0][1])))
     return L
 
 def getTypeVariables(var):
@@ -41,14 +41,14 @@ def lenSequence(x):
     return l
 
 
-def main(crossword_file, crossword_dico):
+def main(crossword_file, crossword_dico, maintain_arc_consistency):
     start_time = time.time()
     dico, letters = parseWords(crossword_dico)
     cells = parseVar(crossword_file)
 
     var = {}
     var1 = {x: set(getCorrectWordLenght(x,dico)) for x in cells if x[1] != x[0]}#It's a sequence in the grid, the possible values are words from the dictionnaries with the same length of the sequence.
-    var2 = {x: set(letters) for x in cells if x[1] == x[0]} #It's a cell in the grid, the possible values are a letter 
+    var2 = {x: set(letters) for x in cells if x[1] == x[0]} #It's a cell in the grid, the possible values are a letter
     for elt in var1:
         var[elt] = var1[elt]
     for elt in var2:
@@ -56,7 +56,7 @@ def main(crossword_file, crossword_dico):
 
 
     P = constraint_programming(var)
-    
+
     def functR3(xj):
         """A sequence is the concatenation of its letters.
            i is a sequence ; j is a letter in the sequence j at the index xj"""
@@ -77,22 +77,18 @@ def main(crossword_file, crossword_dico):
                 P.addConstraint(a,var_letter,functR3(i))
                 i += 1 #the position of the letter within the sequence
 
+    if maintain_arc_consistency == "y":
+        P.maintain_arc_consistency()
     sol = P.solve()
 
     if sol:
         print(sol)
         print("Time of resolution : {} seconds".format(time.time() - start_time))
     else:
-        print("No solution")  
+        print("No solution")
 
 if __name__ == "__main__":
     crossword_file = input("Path of the crossword file: ")
     crossword_dico = input("Path of the crossword dico: ")
-    main(crossword_file, crossword_dico)
-
-                    
-
-
-
-
-
+    maintain_arc_consistency = input("Arc Consistency (y/N) :")
+    main(crossword_file, crossword_dico, maintain_arc_consistency)
